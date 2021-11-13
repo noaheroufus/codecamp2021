35566ae22f090d4cc9a4cb2905c8b8b3f1627402
Stetofire <thekeithrocks@gmail.com>
2021-11-12 22:51:59 -0700

    Add ShrubObject

--- textures.js
+++ textures.js
@@ -5,6 +5,14 @@ const PLAYER_ATTACK = 1;
 const TREE_1 = 2;
 const TREE_2 = 3;
 const TREE_3 = 4;
+const SHRUB_1 = 5;
+const SHRUB_2 = 6;
+const SHRUB_3 = 7;
+const SHRUB_4 = 8;
+const SHRUB_5 = 9;
+const SHRUB_6 = 10;
+const SHRUB_7 = 11;
+const SHRUB_8 = 12;
 
 function LoadTextures(webgl) {
     // Player
@@ -15,4 +23,14 @@ function LoadTextures(webgl) {
     TEXTURES[TREE_1] = new Texture(webgl, TREE_1, "./images/Trees.png", 512, 128, 128, 128, 0, 0);
     TEXTURES[TREE_2] = new Texture(webgl, TREE_2, "./images/Trees.png", 512, 128, 128, 128, 1, 0);
     TEXTURES[TREE_3] = new Texture(webgl, TREE_3, "./images/Trees.png", 512, 128, 128, 128, 2, 0);
+
+    // Shrubs
+    TEXTURES[SHRUB_1] = new Texture(webgl, SHRUB_1, "./images/Shrubs.png", 256, 32, 32, 32, 0, 0);
+    TEXTURES[SHRUB_2] = new Texture(webgl, SHRUB_2, "./images/Shrubs.png", 256, 32, 32, 32, 1, 0);
+    TEXTURES[SHRUB_3] = new Texture(webgl, SHRUB_3, "./images/Shrubs.png", 256, 32, 32, 32, 2, 0);
+    TEXTURES[SHRUB_4] = new Texture(webgl, SHRUB_4, "./images/Shrubs.png", 256, 32, 32, 32, 3, 0);
+    TEXTURES[SHRUB_5] = new Texture(webgl, SHRUB_5, "./images/Shrubs.png", 256, 32, 32, 32, 4, 0);
+    TEXTURES[SHRUB_6] = new Texture(webgl, SHRUB_6, "./images/Shrubs.png", 256, 32, 32, 32, 5, 0);
+    TEXTURES[SHRUB_7] = new Texture(webgl, SHRUB_7, "./images/Shrubs.png", 256, 32, 32, 32, 6, 0);
+    TEXTURES[SHRUB_8] = new Texture(webgl, SHRUB_8, "./images/Shrubs.png", 256, 32, 32, 32, 7, 0);
 }
